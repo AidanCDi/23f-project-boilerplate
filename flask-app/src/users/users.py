7615a1fe541d73e8192148bac8faef65a8823ae6
timclay <clay.t@northeastern.edu>
2023-12-03 14:07:40 -0500

    Remove extraneous route

--- flask-app/src/users/users.py
+++ flask-app/src/users/users.py
@@ -80,37 +80,6 @@ def get_all_plans (user_id):
     return jsonify(json_data)
 
 
-@users.route('/users/<user_id>/plans/<plan_name>', methods=['GET'])
-def get_all_plans (user_id, plan_name):
-
-    query = 'SELECT Title, Price, "Calories per Serving", Servings, Category, PostDate\
-        FROM PlanRecipes pr\
-            JOIN (SELECT r.RecipeID, Title, Price, ROUND(Calories / Servings) as "Calories per Serving", Servings, Name as Category, DATE_FORMAT(DATE(PostDate), "%m/%d/%Y") as PostDate\
-                FROM Recipes r\
-                    JOIN RecipeCategories rc\
-                        ON r.recipeID = rc.RecipeID\
-                    JOIN Categories c\
-                        ON rc.CategoryID = c.CategoryID\
-                    JOIN (SELECT r.RecipeID, sum(ri.Units * i.UnitPrice) as Price, sum(i.UnitCalories) as Calories\
-                            FROM Recipes r\
-                                LEFT OUTER JOIN RecipeIngredients ri on r.RecipeID = ri.RecipeID\
-                                JOIN Ingredients i on ri.IngredientID = i.IngredientID\
-                            GROUP BY r.RecipeID) recipe_attributes\
-                        ON r.RecipeID = recipe_attributes.RecipeID) r2\
-                ON pr.RecipeID = r2.RecipeID\
-        WHERE pr.UserID = ' + str(user_id) + ' AND pr.PlanName = ' + str(plan_name)
-    current_app.logger.info(query)
-
-    cursor = db.get_db().cursor()
-    cursor.execute(query)
-    column_headers = [x[0] for x in cursor.description]
-    json_data = []
-    the_data = cursor.fetchall()
-    for row in the_data:
-        json_data.append(dict(zip(column_headers, row)))
-    return jsonify(json_data)
-
-
 @users.route('/users/<user_id>/plans', methods=['POST'])
 def add_review(user_id):
     
