59bb8049a1c6a1f957673be11e59485ce8c1dfed
timothy-clay <tjc0426@gmail.com>
2023-12-05 20:22:31 -0500

    added plans/list route

--- flask-app/src/users/users.py
+++ flask-app/src/users/users.py
@@ -94,6 +94,22 @@ def get_all_plans (user_id):
     return jsonify(json_data)
 
 
+@users.route('/users/<user_id>/plans/list', methods=['GET'])
+def get_all_plans (user_id):
+
+    query = 'SELECT PlanName as name, PlanName as code FROM Plans WHERE UserID = ' + str(user_id) 
+    current_app.logger.info(query)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    column_headers = [x[0] for x in cursor.description]
+    json_data = []
+    the_data = cursor.fetchall()
+    for row in the_data:
+        json_data.append(dict(zip(column_headers, row)))
+    return jsonify(json_data)
+
+
 @users.route('/users/<user_id>/plans', methods=['POST'])
 def add_review(user_id):
     
