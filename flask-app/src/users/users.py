1fb6eaa1d5443fd379743a6aea7e7bda282e65a1
timothy-clay <tjc0426@gmail.com>
2023-12-05 21:12:40 -0500

    create plan_recipes route

--- flask-app/src/users/users.py
+++ flask-app/src/users/users.py
@@ -94,6 +94,33 @@ def get_all_plans (user_id):
     return jsonify(json_data)
 
 
+@users.route('/users/<user_id>/plans/<plan_name>', methods=['GET'])
+def get_plan_recipes (user_id, plan_name):
+
+    query = "SELECT all_recipes.RecipeID as RecipeID, Title, Description, Price, Calories, Fiber, Protein, Servings, Category, Rating FROM PlanRecipes pr\
+         JOIN (SELECT r.RecipeID as RecipeID, Title, Description, Instructions, Price, ROUND(Calories / Servings) as Calories, ROUND(Fiber / Servings) as Fiber, ROUND(Protein / Servings) as Protein, Servings, Category, DATE_FORMAT(DATE(PostDate), '%m/%d/%Y') as PostDate, Rating\
+        FROM Recipes r\
+            JOIN (SELECT r.RecipeID, sum(ri.Units * i.UnitPrice) as Price, sum(i.UnitCalories) as Calories, sum(i.UnitFiber) as Fiber, sum(i.UnitProtein) as Protein\
+                FROM Recipes r\
+                    LEFT OUTER JOIN RecipeIngredients ri\
+                        ON r.RecipeID = ri.RecipeID\
+                    JOIN Ingredients i on ri.IngredientID = i.IngredientID\
+                GROUP BY r.RecipeID) recipe_attributes\
+                ON r.RecipeID = recipe_attributes.RecipeID\
+                    LEFT OUTER JOIN (SELECT RecipeID, round(avg(Rating), 2) as Rating FROM Reviews GROUP BY RecipeID) reviews ON r.RecipeID = reviews.RecipeID) all_recipes ON pr.RecipeID = all_recipes.RecipeID\
+         WHERE PlanName = " + str(plan_name) + " AND UserID = " + str(user_id)"" 
+    current_app.logger.info(query)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    column_headers = [x[0] for x in cursor.description]
+    json_data = []
+    the_data = cursor.fetchall()
+    for row in the_data:
+        json_data.append(dict(zip(column_headers, row)))
+    return jsonify(json_data)
+
+
 @users.route('/users/<user_id>/plans/list', methods=['GET'])
 def get_all_plans_list (user_id):
 
