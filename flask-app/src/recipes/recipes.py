139c55be0824ae869df64b07ec1d761aaaf6f2ea
timothy-clay <tjc0426@gmail.com>
2023-12-05 23:30:52 -0500

    create put request for review

--- flask-app/src/recipes/recipes.py
+++ flask-app/src/recipes/recipes.py
@@ -405,6 +405,33 @@ def add_review(recipe_id):
     
     return 'Success!'
 
+
+@recipes.route('/recipes/<recipe_id>/reviews', methods=['PUT'])
+def edit_review(recipe_id):
+    
+    # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    #extracting the variable
+    user_id = the_data['user_id']
+    review_content = the_data['review_content']
+    rating = the_data['rating']
+
+    # Constructing the query
+    query = 'UPDATE Reviews SET '
+    query += 'ReviewContent = ' + str(review_content) + '", "'
+    query += 'Rating = ' + str(rating) + '", "'
+    query += 'WHERE UserID = "' + str(user_id) + '" AND RecipeID = "' + str(recipe_id) + '";'
+    current_app.logger.info(query)
+
+    # executing and committing the insert statement 
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+    
+    return 'Success!'
+
     
 
 # # get the top 5 products from the database
