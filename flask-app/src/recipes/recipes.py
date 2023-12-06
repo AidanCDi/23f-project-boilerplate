5d1e747cef9f91e6bd5eb5d2939d493aaffa6b48
timothy-clay <tjc0426@gmail.com>
2023-12-06 03:04:26 -0500

    remove duplicate route

--- flask-app/src/recipes/recipes.py
+++ flask-app/src/recipes/recipes.py
@@ -346,20 +346,24 @@ def add_recipe():
     current_app.logger.info(the_data)
 
     #extracting the variable
-    title = the_data['recipe_title']
-    description = the_data['recipe_description']
-    instructions = the_data['recipe_instructions']
-    preptime = the_data['recipe_preptime']
-    cooktime = the_data['recipe_cooktime']
-    user_id = the_data['user_id']
+    title = the_data['Title']
+    description = the_data['Description']
+    instructions = the_data['Instructions']
+    servings = the_data['Servings']
+    preptime = the_data['PrepTime']
+    cooktime = the_data['CookTime']
+    ingredients = the_data['Ingredients']
+    appliances = the_data['Appliances']
+    user_id = the_data['UserID']
 
     # Constructing the query
-    query = 'insert into Recipes (Title, Instructions, Servings, PrepTime, CookTime, UserTime) values ("'
-    query += title + '", "'
-    query += description + '", "'
-    query += instructions + '", '
-    query += str(preptime) + '", '
-    query += str(cooktime) + '", '
+    query = 'insert into Recipes (Title, Description, Instructions, Servings, PrepTime, CookTime, UserID) values ("'
+    query += str(title) + '", "'
+    query += str(description) + '", "'
+    query += str(instructions) + '", '
+    query += str(servings) + ', '
+    query += str(preptime) + ', '
+    query += str(cooktime) + ', '
     query += str(user_id) + ')'
     current_app.logger.info(query)
 
@@ -491,41 +495,7 @@ def delete_review(recipe_id, review_id):
     return 'Success!'
 
 
-@recipes.route('/recipes', methods=['POST'])
-def add_recipe():
-    
-    # collecting data from the request object 
-    the_data = request.json
-    current_app.logger.info(the_data)
 
-    #extracting the variable
-    title = the_data['Title']
-    description = the_data['Description']
-    instructions = the_data['Instructions']
-    servings = the_data['Servings']
-    preptime = the_data['PrepTime']
-    cooktime = the_data['CookTime']
-    ingredients = the_data['Ingredients']
-    appliances = the_data['Appliances']
-    user_id = the_data['UserID']
-
-    # Constructing the query
-    query = 'insert into Recipes (Title, Description, Instructions, Servings, PrepTime, CookTime, UserID) values ("'
-    query += str(title) + '", "'
-    query += str(description) + '", "'
-    query += str(instructions) + '", '
-    query += str(servings) + ', '
-    query += str(preptime) + ', '
-    query += str(cooktime) + ', '
-    query += str(user_id) + ')'
-    current_app.logger.info(query)
-
-    # executing and committing the insert statement 
-    cursor = db.get_db().cursor()
-    cursor.execute(query)
-    db.get_db().commit()
-    
-    return 'Success!'
     
 
 # # get the top 5 products from the database
