dcf83fdaf7a1f170142d64755b3456d220d7efe4
timothy-clay <tjc0426@gmail.com>
2023-12-06 01:44:02 -0500

    added appliances route

--- flask-app/src/recipes/recipes.py
+++ flask-app/src/recipes/recipes.py
@@ -236,6 +236,21 @@ def get_ingredients ():
     return jsonify(json_data)
 
 
+@recipes.route('/appliances', methods=['GET'])
+def get_appliances ():
+
+    query = 'SELECT ApplianceID as code, Name as name FROM Appliances'
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    column_headers = [x[0] for x in cursor.description]
+    json_data = []
+    the_data = cursor.fetchall()
+    for row in the_data:
+        json_data.append(dict(zip(column_headers, row)))
+    return jsonify(json_data)
+
+
 @recipes.route('/ingredients/<ingredient_id>', methods=['GET'])
 def get_ingredient_attrs (ingredient_id):
 
